2867a87871dc2ffef05c13262a6fdb7ea4e21c4a
Gaurav Kukreja <gaurav.kukreja@exasol.com>
2018-08-14 12:07:38 +0200

    Alt+Shift+[BNM] for answers without the comments

--- CheatSheet.py
+++ CheatSheet.py
@@ -4,36 +4,10 @@ import requests
 import os
 import re
 
-extension_language_dict = {
-    ".py" : "python",
-    ".cpp" : "cpp",
-    ".hpp" : "cpp",
-    ".c" : "c",
-    ".h" : "cpp",
-    ".sh" : "bash",
-    ".css" : "css",
-    ".html" : "html",
-    ".htm" : "html",
-    ".xhtml" : "html",
-    ".jhtml" : "html",
-    ".jsp" : "java",
-    ".jspx" : "java",
-    ".wss" : "java",
-    ".do" : "java",
-    ".action" : "java",
-    ".js" : "javascript",
-    ".pl" : "perl",
-    ".php" : "php",
-    ".php4" : "php",
-    ".php3" : "php",
-    ".phtml" : "php",
-    ".rb" : "ruby",
-    ".swift" : "swift",
-}
-
 syntaxFile_language_dict = {"Packages/Python/Python.sublime-syntax": "python"}
 
 def getAnswer(language, query, recommendationNum = 0, withComments = True):
+    query = re.sub(r"\s+", "+", query)
     recommendationStr = ""
     if (recommendationNum > 0):
         recommendationStr = '/' + str(recommendationNum)
@@ -45,44 +19,43 @@ def getAnswer(language, query, recommendationNum = 0, withComments = True):
 
 class CheatSheetUtils:
     def error_programming_language_unknown(self):
-        sublime.message_dialog("Cheat.sh plugin could not deduce the programming language.\n\nPlease save the file with an appropriate extension or set the syntax from the Command Palette (Cmd + Shift + p) and typing \"Set Syntax:\"")
+        sublime.message_dialog("Cheat.sh plugin could not deduce the programming language.\n\nPlease save the file with an appropriate extension or set the syntax from the Command Palette by pressing Cmd + Shift + p and typing \"Set Syntax:\"")
 
     def error_unsupported_programming_language(self):
         sublime.message_dialog("Cheat.sh plugin does not seem to support this programming language.\n\nPlease contact the developer at gaurav@gauravk.in to include support for this programming language.")
-        
-    def getProgrammingLanguage(self):
-        fname = self.view.file_name()
-        if fname is not None:
-            filename, fileext = os.path.splitext(self.view.file_name());
-            if fileext in extension_language_dict:
-                return extension_language_dict[fileext]
-            else:
-                self.error_unsupported_programming_language()
-                return 
+
+    def getLanguage(self):
+        syntaxFile = self.view.settings().get('syntax') 
+        m = re.search('Packages/\w*/(?P<language>\w*)\.sublime-syntax', syntaxFile)
+        if m:
+            language = m.group("language")
+            return "sublime:" + language.lower()
         else:
-            syntaxFile = self.view.settings().get('syntax')
-            if syntaxFile in syntaxFile_language_dict:
-                return syntaxFile_language_dict[syntaxFile]
+            fname = self.view.file_name()
+            if fname is None:
+                self.error_programming_language_unknown()
+            m = re.search('/?(?!\w*/)*\w*\.(?P<fileext>\w*)$', fname)
+            if m:
+                fileext = m.group("fileext")
+                return fileext.lower()
             else:
-                if syntaxFile == 'Packages/Text/Plain text.tmLanguage':
-                    self.error_programming_language_unknown()
-                else:
-                    self.error_unsupported_programming_language()
-                return None
+                self.error_programming_language_unknown()
 
 class CheatSheetCommand(sublime_plugin.TextCommand, CheatSheetUtils):
-    def run(self, edit):
-        language = self.getProgrammingLanguage()
+    def run(self, edit, with_comments):
+        language = self.getLanguage()
         if language is not None:
             for region in self.view.sel():
                 if not region.empty():
-                    s = self.view.substr(region)
-                    s = re.sub(r'\s+(?!\n)', r'+', s)
-                    self.view.replace(edit, region, getAnswer(language, s))
+                    query = self.view.substr(region)
+                    self.view.replace(edit, region, getAnswer(language, query, 0, with_comments))
+                    self.view.run_command("reindent")
 
 class CheatSheetMultipleSuggestionsCommand(sublime_plugin.TextCommand, CheatSheetUtils):
+    show_result_with_comments = True
+
     def on_done(self, user_input):
-        language = self.getProgrammingLanguage()
+        language = self.getLanguage()
         if language is not None:
             newView = self.view.window().new_file()
             newView.settings().set('auto_indent', False)
@@ -91,28 +64,39 @@ class CheatSheetMultipleSuggestionsCommand(sublime_plugin.TextCommand, CheatShee
             sublime.active_window().focus_view(newView)
             separator = '\n\n' + "-"*80 + '\n' + "-"*80 + '\n' + "-"*80 + '\n\n'
             if sublime.active_window().active_view() == newView:
-                newView.run_command('insert', {"characters": getAnswer(language, user_input)})
+                newView.run_command('insert', {"characters": getAnswer(language, user_input, 0, self.show_result_with_comments)})
                 newView.run_command('insert', {"characters": separator})
-                newView.run_command('insert', {"characters": getAnswer(language, user_input, 1)})
+                newView.run_command('insert', {"characters": getAnswer(language, user_input, 1, self.show_result_with_comments)})
                 newView.run_command('insert', {"characters": separator})
-                newView.run_command('insert', {"characters": getAnswer(language, user_input, 2)})
+                newView.run_command('insert', {"characters": getAnswer(language, user_input, 2, self.show_result_with_comments)})
 
-    def run(self, edit):
-        self.view.window().show_input_panel("Cheat.sh", "", self.on_done, None, None)
+    def run(self, edit, with_comments):
+        self.show_result_with_comments = with_comments
+        input_panel_prompt = "Cheat.sh"
+        if with_comments:
+            input_panel_prompt = input_panel_prompt + " (with comments) :"
+        else:
+            input_panel_prompt = input_panel_prompt + " (without comments) :"
+        self.view.window().show_input_panel(input_panel_prompt, "", self.on_done, None, None)
 
 class CheatSheetInputPanelCommand(sublime_plugin.TextCommand, CheatSheetUtils):
+    show_result_with_comments = True
+
     def on_done(self, user_input):
-        language = self.getProgrammingLanguage()
+        language = self.getLanguage()
         if language is not None:
             old_auto_indent_status = self.view.settings().get('auto_indent')
             if (old_auto_indent_status == True):
                 self.view.settings().set('auto_indent', False)
-            self.view.run_command('insert', {"characters": getAnswer(language, user_input)})
+            self.view.run_command('insert', {"characters": getAnswer(language, user_input, 0, self.show_result_with_comments)})
             if (old_auto_indent_status == True):
                 self.view.settings().set('auto_indent', True)
 
-    def run(self, edit):
-        self.view.window().show_input_panel("Cheat.sh", "", self.on_done, None, None)
-        
-
-
+    def run(self, edit, with_comments):
+        self.show_result_with_comments = with_comments
+        input_panel_prompt = "Cheat.sh"
+        if with_comments:
+            input_panel_prompt = input_panel_prompt + " (with comments) :"
+        else:
+            input_panel_prompt = input_panel_prompt + " (without comments) :"
+        self.view.window().show_input_panel(input_panel_prompt, "", self.on_done, None, None)
